4b3105e86d9531d90c4bdba6df6d566c39253e6f
AlisaEKo <114878172+AlisaEKo@users.noreply.github.com>
2022-11-01 11:24:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const person = ["K", "o", "t", "l", "i", "a", "r", "s"];
+console.log(person[3]);
+
+console.log(person.length);
+
+person[2]= "Alisa";
+console.log(person[2]);
+
+person.push("Tom");
+console.log(person);
+
+const date = new Date();
+console.log(date);
+
+const people = ["Alisa", "Tom", "Ron",];
+people[0] = "Mika";
+console.log(people)
+console.log(people[1]);
+
+const person = {firstName:"Alisa", lastName:"Kotler", age:39};
+console.log(person.firstName);
+
+// Home Work //
+const person = {
+    firstName:"Alisa",
+    lastName:"Kotliarski",
+    age:39,
+    carName:"Suzuki",
+    birthDay:"08/03/1983",
+    fullName:function() {return this.firstName + " " + this.lastName + " " + this.birthDay;}
+};
+console.log(person.fullName());
+
+let birthDay = "08/03/1983";
+console.log(birthDay);
+
+
+const date = new Date(year=2022, month=10, day=01, hours=11, minutes=21);
+console.log(date);
+
+const date = new Date(year=2022, month=10, day=35, hours=11, minutes=21);
+console.log(date);
+
+
+
